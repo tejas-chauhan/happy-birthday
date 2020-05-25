16727e4843fcb52bd58ce57aa8708e40ad6fa89f
Dhruv Doshi <160770107043@socet.edu.in>
2020-05-25 20:32:37 +0530

    main vue "code of home page "

---
+++ code_files/birthday-prachi/src/views/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container grid-list-xl>
+    <v-layout text-center row wrap>
+      <v-flex xs12>
+        <v-img :src="require('../assets/prachi-prof-pic.jpeg')" contain height="200"></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2">Happy Birthday!</h1>
+        <h2 class="display-1">Prachi Kadam </h2>
+      </v-flex>
+
+      <v-flex xs12>
+        <v-img :src="require('../assets/bday-112.gif')" contain height="200"></v-img>
+      </v-flex>
+
+      <v-flex xs12>
+        <v-btn to="/memories" color="info">See Your Memories</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script src="./Main.js">
+</script>
