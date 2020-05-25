a89ebde7d74e4d65246141a152efdb2a1ee9e148
Dhruv Doshi <160770107043@socet.edu.in>
2020-05-25 20:29:50 +0530

    veutify and rouer systems

---
+++ code_files/birthday-prachi/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import vuetify from '@/plugins/vuetify';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  vuetify,
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount('#app');
