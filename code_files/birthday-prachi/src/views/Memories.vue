a389002b80d09b26a2d025d96cd77b36f1cc09c8
Dhruv Doshi <160770107043@socet.edu.in>
2020-05-25 20:33:39 +0530

    Memory page whole structue

---
+++ code_files/birthday-prachi/src/views/Memories.vue
@@ -0,0 +1,252 @@
+<template>
+  <v-container fluid grid-list-xl>
+    <v-layout row text-center wrap>
+      <v-flex xs12>
+        <h1 class="display-3">Our Memories</h1>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-1.jpg')" aspect-ratio="1" ></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline text-xs-center">Friends!!</h1><br>
+              <h2 class="subheading">Best group photo ever!!</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-2.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Favourite festival of Your's</h1><br>
+              <h2 class="subheading">Isme tu aapna khana aaur pina he bhol jaati hai </h2>
+
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-3.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Lag raha hai kise ke shadi hai </h1><br>
+              <h2 class="pannel-body">Jab hum sabne indian kapde he -pehne the</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-4.jpg')" aspect-ratio="1.2"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Superheros!!</h1><br>
+              <h2 class="subheading">Iske aage kuch nahe likh sakte</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-5.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Kya kehna iska ??</h1><br>
+              <h2 class="subheading">Boht maza aye the but next time aaur karenge!!</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-6.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Executives🙄</h1><br>
+              <h2 class="subheading">Do we look like CS Enginners?? 😂</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-7.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Selfies 🤩</h1><br>
+              <h2 class="subheading">Kya shakal banayi hai sab logo ne 😣</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-8.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Favourite food!!</h1><br>
+              <h2 class="subheading">Vender be like sala apun ich Ambani hai aa jao sab😂🤣 </h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-12.jpg')" aspect-ratio="1"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Random College Pic </h1>
+              <h2 class="subheading"> Missing those days, bunking the -lectures!! 😅</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-9.jpg')" aspect-ratio="1.5"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Future will be like ....</h1><br>
+              <h2 class="subheading"> Pata hai kitna wait kia tha humne bahar aapke lie 😶</h2><br>
+              <h2 class="subheading"> Next time aaisa kara toh bilkul --achaa nahe hooga 😂😂 </h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-13.jpg')" aspect-ratio="1.5"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Santsarovar !!</h1><br>
+              <h2
+                class="subheading">Everyone didn't expected that you would come 😂😅</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-14.jpg')" aspect-ratio="1.5"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">First Junoon !!</h1><br>
+              <h2 class="subheading"
+              >Where the hell mota was ?😜</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-15.jpg')" aspect-ratio="1.5"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">The DE Project </h1><br>
+              <h2
+                class="subheading"
+              >Yaad hai isme aapne kitna sara --kam kia tha😂</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-18.jpg')" aspect-ratio="1.5"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">The holi picture 💦</h1><br>
+              <h2
+                class="subheading"
+              >Kitna try kia tha bhagne ka lekin kuch nahe kar payi 🤪</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-20.jpg')" aspect-ratio="1.5"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">The Birthday 🎂</h1><br>
+              <h2
+                class="subheading"
+              >Aab toh party de de kitni sari ----pending hai 😂</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-16.jpg')" aspect-ratio="0.65"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">The last fest of college</h1><br>
+              <h2
+                class="subheading"
+              >Just remember this day for Tejas😂😂😂 </h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-19.jpg')" aspect-ratio="0.65"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">The movie 🎥🎬</h1>
+              <h2
+                class="subheading"
+              >Could you recall the name of the movie ??🤔</h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6 md4>
+        <v-card>
+          <v-img :src="require('../assets/prachi-memory-10.jpg')" aspect-ratio="1.25"></v-img>
+          <v-card-title primary-title>
+            <v-card-text>
+              <h1 class="headline">Last year Birthday</h1><br>
+              <h2 class="subheading">
+                Last year was funn and wish that upcomming one be too !!
+              </h2>
+            </v-card-text>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+
+
+      <v-flex xs12>
+        <v-img :src="require('../assets/bday-159.gif')" contain height="300"></v-img>
+      </v-flex>
+
+      <v-flex xs12>
+        <v-btn to="/words" color="info">This is For You</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
